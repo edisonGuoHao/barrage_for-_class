821d484891c8d30bba5a4907072b49b2807cc910
edison_Kwok <edison_guohao@outlook.com>
2019-06-02 19:05:56 +0800

    version 1.0
    完成基础弹幕功能，分为客户端和服务端，客户端可以向服务端发送弹幕。

---
+++ src/main/java/com/jogeen/barrage/server/data/GiftImage.java
@@ -0,0 +1,51 @@
+package com.jogeen.barrage.server.data;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import com.jogeen.barrage.common.GiftEnum;
+
+/**
+ *  礼物图片加载类
+ * @author jogeen
+ * @version 1.0
+ * @date 2018年10月16日
+ */
+public class GiftImage {
+	private static String path = GiftImage.class.getResource("/").getPath();
+	private static BufferedImage roseImg = null;
+	private static BufferedImage rocketImg = null;
+
+	public static BufferedImage getRoseImage() {
+		if (roseImg == null) {
+			try {
+				roseImg = ImageIO.read(new FileInputStream(path + "/image/rose.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return roseImg;
+	}
+
+	public static BufferedImage getRocketImage() {
+		if (rocketImg == null) {
+			try {
+				rocketImg = ImageIO.read(new FileInputStream(path + "/image/rocket.png"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return rocketImg;
+	}
+	
+	public static BufferedImage getImageByName(String imageName) {
+		if(GiftEnum.ROSE.getType().equals(imageName)) {
+			return getRoseImage();
+		}
+		if(GiftEnum.ROCKET.getType().equals(imageName)) {
+			return getRocketImage();
+		}
+		return null;
+	}
+
+}
